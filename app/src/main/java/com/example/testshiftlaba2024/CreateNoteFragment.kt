4d079b7a4b17b846b6aae944e3a6b41facae42c1
Chelyapina <katechelya@gmail.com>
2024-10-19 23:18:54 +0700

    Ui

---
+++ app/src/main/java/com/example/testshiftlaba2024/CreateNoteFragment.kt
@@ -0,0 +1,19 @@
+package com.example.testshiftlaba2024
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CreateNoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_create_note, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/testshiftlaba2024/EditNoteFragment.kt
@@ -0,0 +1,19 @@
+package com.example.testshiftlaba2024
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class EditNoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+    }
+
+}
