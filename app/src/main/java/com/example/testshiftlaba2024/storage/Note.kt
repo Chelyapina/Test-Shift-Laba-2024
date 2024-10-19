09494277a7a42bf64d09e5c89512d6f2dd4a881b
Chelyapina <katechelya@gmail.com>
2024-10-20 00:24:09 +0700

    Db

---
+++ app/src/main/java/com/example/testshiftlaba2024/storage/Note.kt
@@ -0,0 +1,15 @@
+package com.example.testshiftlaba2024.storage
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    var noteId: Long = 0L,
+    @ColumnInfo(name = "task_title")
+    var noteTitle: String = "",
+    @ColumnInfo(name = "task_body")
+    var noteBody: String = ""
+)
---
+++ app/src/main/java/com/example/testshiftlaba2024/storage/NoteDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.testshiftlaba2024.storage
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract val noteDao : NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE : NoteDatabase?= null
+
+        fun getInstance(context : Context) : NoteDatabase {
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteDatabase::class.java,
+                        "notes_database"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+}
